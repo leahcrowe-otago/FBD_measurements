030436f82fd9e6265ceec482b8310a0610b71a3c
leahcrowe-otago <leah.crowe2postgrad.otago.ac.nz>
2024-01-18 16:47:41 +1300

    figured out i and j. copied to rmd file because error messages weren't displaying here for some reason.

--- scripts/individuals_vbgc.R
+++ scripts/individuals_vbgc.R
@@ -4,30 +4,57 @@ head(age_vbgc)
 
 ij<-age_vbgc%>%
     arrange(ID, age_month)%>%
+  ungroup()%>%
+    mutate(ind = 1:n())%>%
     group_by(ID)%>%
-    mutate(j = 1:n())%>%
-    arrange(ID)%>%
-    group_by(ID)%>%
-    mutate(i = cur_group_id())%>%
-    ungroup(i, j)%>%
-    mutate(J = max(j))%>%
-    dplyr::select(ID, i, J, j, age_month, length)
-
-#individuals max num captures (J)    
-iJ<-ij%>%
-  distinct(iJ)
-  
-summary(ij)
+    mutate(j = 1:n(),
+           J = max(ind),
+           i = cur_group_id() 
+           )%>%
+    #arrange(ID)%>%
+    #group_by(ID)%>%
+    #mutate(i = cur_group_id())%>%
+    #mutate(J = max(j))%>%
+    dplyr::select(ID, i, ind, j, J, age_month, length)%>%
+  ungroup()
 
 head(ij)
 tail(ij)
+summary(ij)
+
+age_ij<-ij%>%
+  dplyr::select(ID, j, age_month)%>%
+  group_by(ID)%>%
+  tidyr::pivot_wider(names_from = j, values_from = age_month)%>%
+  ungroup()%>%
+  dplyr::select(-ID)
+
+colnames(age_ij)<-NULL
+age_ij
+  
+length_ij<-ij%>%
+  dplyr::select(ID, j, length)%>%
+  group_by(ID)%>%
+  tidyr::pivot_wider(names_from = j, values_from = length)%>%
+  ungroup()%>%
+  dplyr::select(-ID)
+
+colnames(length_ij)<-NULL
+as.matrix(length_ij)
+
 #number of individuals
-n = max(iJ$i)
+#n = max(ij$i)
+n = nrow(age_ij)
+n
+nrow(length_ij)
+
+J = max(ij$j)
+J
 
 model<-nimble::nimbleCode({
   
   for (i in 1:n){ 
-    
+
     for(j in 1:J){
     
     #likelihood
@@ -35,25 +62,29 @@ model<-nimble::nimbleCode({
     # y = y[i](t[j]) eq. 1 observed length
     # x = g[i](t[j]) = mean distribution specified in terms of the VB growth curve
     
-    #Schofield et al. 2013 eq. 1
+     #Schofield et al. 2013 eq. 1
     y[i,j] ~ T(dnorm(x[i,j], tau.y), 0, Inf)
-    #heirarchical
-    Linf[i] ~ T(dnorm(beta_l0 + beta_l1, tau.Linf), 0, Inf)
-    logit(K[i]) ~ dnorm(beta_k0 + beta_k1, tau.K)      
+    #y[i] ~ T(dnorm(x[i], tau.y), 0, Inf)
+     #Schofield heirarchical
+    Linf[i] ~ T(dnorm(beta_l, tau.Linf), 0, Inf)
+    logit(K[i]) ~ dnorm(beta_k, tau.K)
+    log(t0) ~ dnorm(beta_t0, tau.t0)
       #Schofield et al. 2013 eq. 3
     x[i,j] <- Linf[i]*(1 - K[i]^(age[i,j] - t0))
+    #x[i] <- Linf[i]*(1 - K[i]^(age[i] - t0))
     
     #Midway et al. 2015
     #y[i] ~ dnorm(y.hat[i], tau.y)
     #y.hat[i] <- Linf *(1 - exp(-K *(age[i] - t0)))
     }
-  }  
+  }
   #prior
   
-  beta_l0 ~ dnorm(0, 1000)
-  beta_l1 ~ dnorm(0, 1000)
-  beta_k0 ~ dlogis(0,1)
-  beta_k1 ~ dlogis(0,1)
+  beta_l ~ dnorm(0, 1000)
+  #beta_l1 ~ dnorm(0, 1000)
+  beta_k ~ dlogis(0, 1)
+  #beta_k1 ~ dlogis(0,1)
+  beta_t0 ~ dnorm(0, 5)
   
   #Midway et al. 2015
   tau.y <- pow(sigma_y, -2)
@@ -65,35 +96,38 @@ model<-nimble::nimbleCode({
   tau.K <- pow(sigma_K, -2)
   sigma_K ~ dunif(0, 5)
   
+  tau.t0 <- pow(sigma_t0, -2)
+  sigma_t0 ~ dunif(0, 5)
+  
   #remove Linf and K from priors for heirarchical model
   #Linf ~ dnorm(3, 0.4)
   #K ~ dunif(0, 1)
-  t0 ~ dnorm(-2.5, 1)
+  #t0 ~ dnorm(-2.5, 1)
   
-})
+  })
 
 inits_fun = function(){
   
   #K_init = 0.5
-  t0_init = -2.5
+  #t0_init = -2.5
   
   
-  return(list(t0 = t0_init, beta_10 = 0, beta_l1 = 0, beta_k0 = 0, beta_k1 = 0))
+  return(list(beta_1 = 0, beta_k = 0, beta_t0 = 0))
   
 }
 #Midway
 #params = c("Linf", "K", "t0", "sigma_y", "y.hat")
 #Schofield
-params = c("Linf", "K", "t0", "sigma_y", "x")
+params = c("Linf", "K", "t0", "sigma_y")
 
-data = list(y = as.numeric(ij$length), 
-            age = as.numeric(ij$age_month),
-            J = as.numeric(iJ$J),
-            i = as.numeric(ij$i))
+list(y = matrix(seizures$seize, ncol = J, nrow = n, byrow = TRUE))
+
+data = list(y = as.matrix(length_ij), 
+            age = as.matrix(age_ij))
 
 
 Rmodel <- nimble::nimbleModel(code = model,
-                              constants = list(n = n),
+                              constants = list(n = n, J = J),
                               data = data,
                               inits = inits_fun())
 
@@ -106,7 +140,8 @@ coda.samples<-coda::mcmc(samples)
 
 summ<-summary(samples)
 summ
-summ$statistics[1]
+samples.df<-as.data.frame(as.matrix(samples))
+summ$statistics[805]
 summ$quantiles[c(1,5)]
 
 bayesplot::mcmc_trace(coda.samples)
