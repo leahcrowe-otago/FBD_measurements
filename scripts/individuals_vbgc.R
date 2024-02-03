0a8aaa6261c63a56e7e50efeecf4a9560dbceed7
leahcrowe-otago <leah.crowe2postgrad.otago.ac.nz>
2024-02-03 13:12:13 +1300

    looks pretty good after getting rid of beta and tau for t0p

--- scripts/individuals_vbgc.R
+++ scripts/individuals_vbgc.R
@@ -34,6 +34,7 @@ age_ij<-ij%>%
 
 colnames(age_ij)<-NULL
 as.matrix(age_ij)[52,6]
+saveRDS(age_ij, file = "./data/age_ij.rds")
 
 length_ij<-ij%>%
   dplyr::select(ID, j, length)%>%
@@ -44,6 +45,7 @@ length_ij<-ij%>%
 
 colnames(length_ij)<-NULL
 as.matrix(length_ij)[52,6]
+saveRDS(age_ij, file = "./data/length_ij.rds")
 
 max_ind_j<-ij%>%
   dplyr::select(ID, j)%>%
@@ -77,41 +79,39 @@ model<-nimble::nimbleCode({
       #Schofield heirarchical
       Linf[i] ~ dnorm(beta_Linf, tau = tau_Linf) #constrain to positive
       logit(K[i]) ~ dnorm(beta_K, tau = tau_K) # K = e^-k, constrain to 0, 1
-      log(t0p) ~ dnorm(beta_t0p, tau = tau_t0p) # throw a negative on here to get t0 proper
-      
+
       #Schofield et al. 2013 eq. 3
-      x[i,j] <- Linf[i]*(1 - K[i]^(age[i,j] + t0p))
+      x[i,j] <- Linf[i]*(1 - (K[i]^(age[i,j] + t0p)))
       
     }
   }
   #prior
   
-  beta_Linf ~ dnorm(0, sd = 1000)
+  beta_Linf ~ dnorm(0, pow(1000, -2))
   beta_K ~ dlogis(0, 1)
-  beta_t0p ~ dlnorm(0, 1)
+  #beta_t0p ~ dlnorm(0, 1)
   
   tau_y <- pow(sigma_y, -2)
-  sigma_y ~ T(dnorm(0, sd = 50), 0, Inf)
+  sigma_y ~ T(dt(0, pow(50, -2), df= 3), 0, Inf)
   
   tau_Linf <- pow(sigma_Linf, -2)
-  sigma_Linf ~ T(dnorm(0, sd = 50), 0, Inf)
+  sigma_Linf ~ T(dt(0, pow(50, -2), df= 3), 0, Inf)
   
   tau_K <- pow(sigma_K, -2)
-  sigma_K ~ T(dnorm(0, sd = 5), 0, Inf)
+  sigma_K ~ T(dt(0, pow(5, -2), df= 3), 0, Inf)
   
-  tau_t0p <- pow(sigma_t0p, -2)
-  sigma_t0p ~ T(dnorm(0, sd = 5), 0, Inf)
-  
+  log(t0p) ~ dnorm(0, 0.1) # throw a negative on here to get t0 proper
+
   })
 
 inits_fun = function(){
   
-  return(list(beta_Linf = 0, beta_K = 0, beta_t0p = 1))
+  return(list(beta_Linf = 0, beta_K = 0))
   
 }
 
 
-params = c("Linf", "K", "t0p", "sigma_y", "beta_Linf", "sigma_Linf", "beta_K", "sigma_K", "beta_t0p", "sigma_t0p")
+params = c("Linf", "K", "t0p", "sigma_y", "beta_Linf", "sigma_Linf", "beta_K", "sigma_K")
 
 data = list(y = as.matrix(length_ij), 
             age = as.matrix(age_ij))
@@ -145,10 +145,11 @@ samples<-nimble::nimbleMCMC(code = model,
 #### -----
 coda.samples<-coda::mcmc(samples)
 
-summ<-summary(samples)
-summ
-samples.df<-as.data.frame(as.matrix(samples))
-
 coda::gelman.diag(coda.samples)
-bayesplot::mcmc_trace(coda.samples, pars = c("beta_Linf", "beta_K", "beta_t0p", "sigma_Linf", "sigma_K", "sigma_t0p"))
+bayesplot::mcmc_trace(coda.samples, pars = c("beta_Linf", "beta_K", "sigma_Linf", "sigma_K", "sigma_y", "t0p"))
 
+
+summ<-summary(samples)
+head(summ$statistics)
+mean_summ_df<-as.data.frame(summ$statistics)
+samples.df<-as.data.frame(as.matrix(samples))
