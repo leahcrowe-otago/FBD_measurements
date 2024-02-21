40b1d56ffbad848b78d5cc415d97c5051afa0fa6
leahcrowe-otago <leah.crowe2postgrad.otago.ac.nz>
2024-02-21 16:17:29 +1300

    model for different dataframes, Sigma out

--- scripts/stan/runstan_allo_indexing.R
+++ scripts/stan/runstan_allo_indexing.R
@@ -3,7 +3,7 @@ library(posterior)
 library(bayesplot)
 library(latex2exp)
 library(dplyr)
-i = 785
+i = 18
 ### get data
 #both bhdf & tl
 ij_b = readRDS(file = 'ij_1.rds')
@@ -12,43 +12,27 @@ ij_b[i,]
 N_b = nrow(ij_b)
 N_b
 #bhdf only
-ij_z = readRDS(file = 'ij_2.rds')
+ij_z = readRDS(file = 'ij_2.rds')%>%
+  dplyr::select(-length)
 #ij_z = as.matrix(ij_z)
 ij_z[i,]
 N_z = nrow(ij_z)
 N_z
 
 #tl only
-ij_y = readRDS(file = 'ij_3.rds')
+ij_y = readRDS(file = 'ij_3.rds')%>%
+  dplyr::select(-BHDF)
 #ij_y = as.matrix(ij_y)
 ij_y[i,]
 N_y = nrow(ij_y)
 N_y
 
-ij_NA<-ij_z%>%
-  bind_rows(ij_b)%>%
-  bind_rows(ij_y)
+#IDs
+ij_ID = readRDS(file = 'ij_ID.rds')
 
-ij<-ij_NA
-ij[is.na(ij)] = 0
+n_ind<-nrow(ij_ID)
 
-ij[i+1,]
-N_ij<-nrow(ij)
-
-n_ind<-length(unique(ij$ind))
-max(ij$ind)
-
-#this might have to happen separately?
-ij%>%
-  group_by(ind)%>%
-  mutate(j = max(obs))
-
-
-ij%>%
-  filter(BHDF > 0)%>%
-  distinct(ind)%>%
-  tally()
-
+#params matrix dims
 J=4
 
 # ----
@@ -57,15 +41,25 @@ a = "
 
 data {
   int<lower=0> n_ind; // number individuals
-  int<lower=0> N_ij; // number of measurement rows
-  int<lower=0> N_b; // number of times when tl and bhdf measurements were taken
+  int<lower=0> N_b; // number of times when tl and bhdf measurements were both taken
   int<lower=0> N_z; // number of times when only bhdf measurements were taken
   int<lower=0> N_y; // number of times when only tl measurements were taken
   int<lower=0> J; // number of params (Ly, ky, Lz, kz), dim of multivariate matrix
-  array[N_ij] int ind; //the individuals
-  vector[N_ij] age; // the age observations
-  vector[N_ij] y; // the total length observations
-  vector[N_ij] z; // the bhdf length observations
+  
+  array[N_b] int ind_b; //the individuals
+  array[N_y] int ind_y; //the individuals
+  array[N_z] int ind_z; //the individuals
+  
+  vector[N_b] age_b; // the age observations when tl and bhdf measurements were both taken
+  vector[N_b] y_b; // the total length observations
+  vector[N_b] z_b; // the bhdf length observations
+  
+  vector[N_y] age_y; // the age observations when only tl measurements were taken
+  vector[N_y] y_y; // the total length observations when only tl measurements were taken
+  
+  vector[N_z] age_z; // the age observations when only bhdf measurements were taken
+  vector[N_z] z_z; // the bhdf length observationswhen only bhdf measurements were taken
+  
 }
 
 parameters {
@@ -80,45 +74,48 @@ parameters {
 
 transformed parameters{
 
-
-
 }
 
 
 model {
 
- for(i in 1:n_ind){
 
+ for(i in 1:n_ind){
       par[i] ~ multi_normal_cholesky(mu,diag_pre_multiply(sigma,L));
-  
- }
- 
-  
- for(i in 1:N_ij){
-    if (i <= N_z) {
-      z[i] ~ normal(par[ind[i],3]*(1-inv_logit(par[ind[i],4])^(age[i] + t0p)), sigma_obs[2]);
+      }
+
+
+ for (i in 1:N_y){
+      y_y[i] ~ normal(par[ind_y[i],1]*(1-inv_logit(par[ind_y[i],2])^(age_y[i] + t0p)), sigma_obs[1]);
+      }
+      
+ for (i in 1:N_z) {
+      z_z[i] ~ normal(par[ind_z[i],3]*(1-inv_logit(par[ind_z[i],4])^(age_z[i] + t0p)), sigma_obs[2]);
+      } 
     
-    } else if (i > N_b+N_z){
-      y[i] ~ normal(par[ind[i],1]*(1-inv_logit(par[ind[i],2])^(age[i] + t0p)), sigma_obs[1]);
-      
-    } else {
-      y[i] ~ normal(par[ind[i],1]*(1-inv_logit(par[ind[i],2])^(age[i] + t0p)), sigma_obs[1]);
-      z[i] ~ normal(par[ind[i],3]*(1-inv_logit(par[ind[i],4])^(age[i] + t0p)), sigma_obs[2]);
-      
-    }
-    }
+ for (i in 1:N_b){
+      y_b[i] ~ normal(par[ind_b[i],1]*(1-inv_logit(par[ind_b[i],2])^(age_b[i] + t0p)), sigma_obs[1]);
+      z_b[i] ~ normal(par[ind_b[i],3]*(1-inv_logit(par[ind_b[i],4])^(age_b[i] + t0p)), sigma_obs[2]);
+      }
+
 
 //priors
   t0p ~ lognormal(0, 10);
   mu ~ normal(0, 1000);
   sigma ~ student_t(3, 0, 25);
   L ~ lkj_corr_cholesky(1);
+  
+  for (i in 1:2){
+  sigma_obs[i] ~ student_t(3, 0, 50);
+  }
 
 }
 
 
 generated quantities {
 
+  corr_matrix[J] Sigma;
+  Sigma = multiply_lower_tri_self_transpose(L);
 
 }
 
@@ -157,24 +154,35 @@ init_vb = function(){
   sigma_obs = rlnorm(2)
 
 
+
+
   return(list(t0p = t0p, 
-              L = L, mu = mu, sigma = sigma,
-              par = par, sigma_obs = sigma_obs
+              L = L, mu = mu, sigma = sigma, par = par, 
+              sigma_obs = sigma_obs
               ))
 }
 
 fit_vb <- stan_fit$sample(
   data = list(
     
-    y = ij$length,
-    z = ij$BHDF,
-    age = ij$age,
+    ind_b = ij_b$ind,
+    y_b = ij_b$length,
+    z_b = ij_b$BHDF,
+    age_b = ij_b$age,
+    
+    ind_y = ij_y$ind,
+    y_y = ij_y$length,
+    age_y = ij_y$age,
+    
+    ind_z = ij_z$ind,
+    z_z = ij_z$BHDF,
+    age_z = ij_z$age,
+    
     N_b = N_b,
     N_z = N_z,
     N_y = N_y,
-    N_ij = N_ij,
+    
     n_ind = n_ind,
-    ind = ij$ind,
     J = J
     
   ),
