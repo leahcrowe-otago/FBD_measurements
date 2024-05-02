102fbf31862f7670fadd978faaa1f6d635bf3590
leahcrowe-otago <leah.crowe2postgrad.otago.ac.nz>
2024-05-03 09:22:10 +1200

    box plot update

--- scripts/TtPacificUS.R
+++ scripts/TtPacificUS.R
@@ -4,6 +4,8 @@ TtUSPac<-read_excel('./data/2006-2022 Tt (US Pac).xlsx', sheet = "2006-2022 Tt (
 
 str(TtUSPac)
 
+head(TtUSPac)
+
 unique(TtUSPac$Species)
 unique(TtUSPac$State)
 unique(TtUSPac$`Latitude Units`)
@@ -38,8 +40,12 @@ ggplot(TtUSPac_confirmed)+
   geom_point(aes(x = length_cm, y = latitude_dd, color = State))
 
 ggplot(TtUSPac_confirmed)+
-  geom_boxplot(aes(x = as.factor(latitude_dd), y = length_cm))+
-  facet_wrap(~`Age Class`)
+  geom_boxplot(aes(y = as.factor(latitude_dd), x = length_cm, fill = State), alpha = 0.3)+
+  geom_point(aes(y = as.factor(latitude_dd), x = length_cm, color = State), size = 2)+
+  theme_bw()+
+  ylab("Latitude degree")+
+  xlab("Length (cm)")
+  #facet_wrap(~`Age Class`)
 
 max(TtUSPac_confirmed$latitude_dd)
 min(TtUSPac_confirmed$`Observation Date`)
